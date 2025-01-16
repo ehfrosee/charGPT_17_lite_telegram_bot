442acd3574b510f3c2df9eb8e18d8fc9271a3539
ehfrosee <ehfros.e.e@gmail.com>
2025-01-17 02:14:51 +0300

    Задане pro

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from telegram import InlineKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
 
@@ -7,10 +8,36 @@ load_dotenv()
 
 # токен бота
 TOKEN = os.getenv('TG_TOKEN')
+language = "None"
+button_reply = {"RU":"Вы выбрали русский язык","EN":"You've chosen english"}
+text_reply = {"RU": "Текстовое сообщение получено!", "EN": "We’ve received a message from you!", "None": "Выберете язык интерфейса"}
+voce_reply = {"RU": "Голосовое сообщение получено", "EN": "We’ve received a voice message from you!", "None": "Выберете язык интерфейса"}
+image_reply = {"RU": "Фотография сохранена", "EN": "Photo saved!", "None": "Выберете язык интерфейса"}
+# INLINE
+# форма inline клавиатуры
+inline_frame = [[InlineKeyboardButton("English", callback_data="EN")],
+                [InlineKeyboardButton("Русский", callback_data="RU")]]
+# создаем inline клавиатуру
+inline_keyboard = InlineKeyboardMarkup(inline_frame)
 
 # функция-обработчик команды /start
-async def start(update, context):
-    await update.message.reply_text("Добро пожаловать, мой дорогой друг!")
+async def start(update: Update, _):
+
+    # прикрепляем inline клавиатуру к сообщению
+    await update.message.reply_text('Выберите язык интерфейса', reply_markup=inline_keyboard)
+
+
+# функция-обработчик нажатий на кнопки
+async def button(update: Update, _):
+    # получаем callback query из update
+    query = update.callback_query
+
+    # всплывающее уведомление
+    await query.answer('OK!')
+    global language
+    language = query.data
+    # редактируем сообщение после нажатия
+    await query.edit_message_text(text=button_reply[language])
 
 
 # функция-обработчик команды /help
@@ -20,7 +47,24 @@ async def help(update, context):
 
 # функция-обработчик текстовых сообщений
 async def text(update, context):
-    await update.message.reply_text("Текст, текст, текст...")
+
+    await update.message.reply_text(text_reply[language])
+
+
+# функция-обработчик сообщений с изображениями
+async def image(update, context):
+    await update.message.reply_text(image_reply[language])
+
+    # получаем изображение из апдейта
+    file = await update.message.photo[-1].get_file()
+
+    # сохраняем изображение на диск
+    await file.download_to_drive("./photos/image.jpg")
+
+
+# функция-обработчик голосовых сообщений
+async def voice(update, context):
+    await update.message.reply_text(voce_reply[language])
 
 def main():
 
@@ -31,11 +75,17 @@ def main():
     # добавляем обработчик команды /start
     application.add_handler(CommandHandler("start", start))
 
-    # добавляем обработчик команды /help
-    application.add_handler(CommandHandler("help", help))
-
     # добавляем обработчик текстовых сообщений
     application.add_handler(MessageHandler(filters.TEXT, text))
+
+    # добавляем обработчик сообщений с фотографиями
+    application.add_handler(MessageHandler(filters.PHOTO, image))
+
+    # добавляем обработчик голосовых сообщений
+    application.add_handler(MessageHandler(filters.VOICE, voice))
+
+    # добавляем CallbackQueryHandler (только для inline кнопок)
+    application.add_handler(CallbackQueryHandler(button))
 
     # запускаем бота (нажать Ctrl-C для остановки бота)
     application.run_polling()
